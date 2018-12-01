4c0c207fbc52183366bc950ed8cfb981e6c33428
Camille Hall <camille@creativeambition.net>
2018-12-01 02:09:40 -0800

    Updates

---
+++ precache-manifest.eeffacd4f25e4964229eccc76415b48b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2146e41467852e3cf8952a79fa234fb",
+    "url": "/bands-on-demand/static/media/pacifico.c2146e41.woff2"
+  },
+  {
+    "revision": "6b6170fe52fb23f505b4e056fefd2679",
+    "url": "/bands-on-demand/static/media/pacifico.6b6170fe.ttf"
+  },
+  {
+    "revision": "61700ad38d80864e5e4666dd12ea33e5",
+    "url": "/bands-on-demand/static/media/pacifico.61700ad3.woff"
+  },
+  {
+    "revision": "9159cf07494cc0df2a7b",
+    "url": "/bands-on-demand/static/js/runtime~main.9159cf07.js"
+  },
+  {
+    "revision": "11c4bea2acb3b26b5aef",
+    "url": "/bands-on-demand/static/js/main.11c4bea2.chunk.js"
+  },
+  {
+    "revision": "f7cb4361d10647a0b53f",
+    "url": "/bands-on-demand/static/js/1.f7cb4361.chunk.js"
+  },
+  {
+    "revision": "11c4bea2acb3b26b5aef",
+    "url": "/bands-on-demand/static/css/main.33297187.chunk.css"
+  },
+  {
+    "revision": "f9d65c3c29f0cf71d1a6117537a02f47",
+    "url": "/bands-on-demand/index.html"
+  }
+];
