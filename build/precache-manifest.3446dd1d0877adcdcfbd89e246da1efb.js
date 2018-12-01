efb8eec9ff6e7f88d44fe369bfb3df22886dc834
Camille Hall <camille@creativeambition.net>
2018-12-01 01:26:17 -0800

    react gh page

---
+++ build/precache-manifest.3446dd1d0877adcdcfbd89e246da1efb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2146e41467852e3cf8952a79fa234fb",
+    "url": "/static/media/pacifico.c2146e41.woff2"
+  },
+  {
+    "revision": "6b6170fe52fb23f505b4e056fefd2679",
+    "url": "/static/media/pacifico.6b6170fe.ttf"
+  },
+  {
+    "revision": "61700ad38d80864e5e4666dd12ea33e5",
+    "url": "/static/media/pacifico.61700ad3.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb1d6d17fc290ec4592e",
+    "url": "/static/js/main.bb1d6d17.chunk.js"
+  },
+  {
+    "revision": "f7cb4361d10647a0b53f",
+    "url": "/static/js/1.f7cb4361.chunk.js"
+  },
+  {
+    "revision": "bb1d6d17fc290ec4592e",
+    "url": "/static/css/main.01a29779.chunk.css"
+  },
+  {
+    "revision": "1689fc8917fdeb65336dba903a136a74",
+    "url": "/index.html"
+  }
+];
